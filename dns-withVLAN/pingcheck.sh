7b1a5960087efc0f343ae23ec63a1686ca5f9b8a
Franklin Davis <fdapjkdevops@gmail.com>
2024-01-29 22:55:09 -0500

    Adding configuration for dns-withVLAN.

---
+++ dns-withVLAN/pingcheck.sh
@@ -0,0 +1,5 @@
+#ansible -i hosts -m ping --private-key .ssh/id_ed25519_ansiuser2 dns2
+#ansible -i hosts -m ping dns2
+#After adding the dns2 to the /etc/ansible/hosts, I can perform
+ansible -m ping dnsVlan1
+
